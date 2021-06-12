2c6341e3ff79440e09914231f9294c63080c817f
Tung Nguyen <NAP-BN-Test>
2021-06-12 17:17:08 +0700

    update

--- api/controllers/import_file.js
+++ api/controllers/import_file.js
@@ -239,6 +239,7 @@ module.exports = {
         try {
             let body = req.body;
             var data = JSON.parse(body.data.replace(/!!@@##/g, '&'));
+            var data = JSON.parse(body.data.replace(/%2F/g, '%2F%'));
             database.connectDatabase().then(async db => {
                 for (var i = 0; i < data.length; i++) {
                     if (!data[i]['Nhóm cấp 1']) continue;
